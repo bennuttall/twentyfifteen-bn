551540e4fbcdc6d03bfbec9e3e2e14889ad24016
Ben Nuttall <ben@bennuttall.com>
2015-03-08 04:08:20 +0000

    Add Google+ as publisher

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 	<meta name="viewport" content="width=device-width">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+	<link rel="publisher" href="https://plus.google.com/+BenNuttall" />
 	<!--[if lt IE 9]>
 	<script src="<?php echo esc_url(get_template_directory_uri()); ?>/js/html5.js"></script>
 	<![endif]-->
