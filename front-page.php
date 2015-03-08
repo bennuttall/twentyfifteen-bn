d13a378391d89bfb8e6d4d157578cd9e230ccbb6
Ben Nuttall <ben@bennuttall.com>
2015-03-08 14:04:45 +0000

    Front page wip

---
+++ front-page.php
@@ -0,0 +1,35 @@
+<?php get_header(); ?>
+
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+
+        <?php the_post(); ?>
+
+            <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                <?php twentyfifteen_post_thumbnail(); ?>
+
+                <header class="entry-header">
+                    <?php the_title('<h1 class="entry-title">', '</h1>'); ?>
+                </header>
+
+                <div class="entry-content">
+                    <?php the_content(); ?>
+
+                    <div class="me-elsewhere">
+                        <ul class="grid">
+                            <li class="rpi"><a href="http://www.raspberrypi.org/">Raspberry Pi</a></li>
+                            <li class="github"><a href="https://github.com/bennuttall">GitHub</a></li>
+                            <li class="twitter"><a href="https://twitter.com/">Twitter</a></li>
+                            <li class="flickr"><a href="http://www.flickr.com/photos/ben_nuttall/">Flickr</a></li>
+                        </ul>
+                    </div>
+                </div>
+
+                <?php edit_post_link(__('Edit', 'twentyfifteen'), '<footer class="entry-footer"><span class="edit-link">', '</span></footer><!-- .entry-footer -->'); ?>
+
+            </article>
+
+        </main>
+    </div>
+
+<?php get_footer();
